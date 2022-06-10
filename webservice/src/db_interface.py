4d41cf0b589acb0daa07e34722c4464e35c38ee1
Nick Exadaktylos <nick6finger@gmail.com>
2022-06-10 16:06:27 +0300

    implement login-logout

---
+++ webservice/src/db_interface.py
@@ -0,0 +1,18 @@
+from copyreg import constructor
+
+
+import os
+from pymongo import MongoClient
+
+
+class DBHandle:
+    def __init__(self):
+        mongodb_hostname = os.environ.get("MONGO_HOSTNAME", "localhost")
+        client = MongoClient('mongodb://'+mongodb_hostname+':27017/')
+        db = client["DigitalNotes"]
+
+        self.users = db["users"]
+        self.notes = db["notes"]
+
+    def find_user_by_username(self, username, password):
+        return self.users.find_one({"username": username, "password": password})
